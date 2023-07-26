adde0601028055908413d5a9d7841dad1d0f096f
Bruno Amezcua <amezcua_bruno@hotmail.com>
2023-07-26 02:59:17 -0600

    #BACKEND - Added C for CRUD!

--- backend/app.ts
+++ backend/app.ts
@@ -21,6 +21,7 @@ app.listen(port, () => {
 });
 
 app.get('/products', async (req: Request, res: Response) => {
+  console.log('----- GET -----');
   try {
     const snapData: productModel[] = [];
     const snapshot = await _db.collection('products').get();
@@ -28,8 +29,49 @@ app.get('/products', async (req: Request, res: Response) => {
       const data = doc.data();
       snapData.push({ id: doc.id, name: data.name, desc: data.desc, price: data.price });
     })
-    res.status(200).send({ msg: "Ok", snapData})
+    res.status(200).send({ msg: "Ok", snapData});
+  } catch(error) {
+    res.status(500).send({ msg: "Whoops! Something died", error});
+  }
+});
+
+app.post('/products', async(req: Request, res: Response) => {
+  console.log('----- POST -----');
+  try {
+    const { name, desc, price } = req.body;
+    if(name.length < 3 || name.length > 100) {
+      res.status(403).send({ msg: "Name must be between 3 and 100 chars!" });
+    } else if (desc.length < 5 || desc.length > 1000) {
+      res.status(403).send({ msg: "Description must be between 5 and 1000 chars!"});
+    } else if (price < 1 || price > 20000) {
+      res.status(403).send({ msg: "Price must be in the range of 1 and 20,000!"})
+    } else {
+      const snapshot = await _db.collection('products').add({
+        name,
+        desc,
+        price
+      });
+      res.status(200).send({ msg: "Ok", product: { id: snapshot.id,  body: req.body }});
+    }
   } catch(error) {
     res.status(500).send({ msg: "Whoops! Something died", error})
   }
-})
+});
+
+app.put('/products', async(req: Request, res: Response) => {
+  console.log('----- PUT -----');
+  try {
+    res.status(200).send({ msg: "Ok", todo: 'MISSING IMPLEMENTATION!'});
+  } catch(error) {
+    res.status(500).send({ msg: "Whoops! Something died", error})
+  }
+});
+
+app.delete('/products', async(req: Request, res: Response) => {
+  console.log('----- DELETE -----');
+  try {
+    res.status(200).send({ msg: "Ok", todo: 'MISSING IMPLEMENTATION!'});
+  } catch(error) {
+    res.status(500).send({ msg: "Whoops! Something died", error})
+  }
+});
--- backend/dist/app.js
+++ backend/dist/app.js
@@ -27,6 +27,7 @@ app.listen(port, () => {
     return console.log(`Backend works! Listening on port: ${port}`);
 });
 app.get('/products', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log('----- GET -----');
     try {
         const snapData = [];
         const snapshot = yield _db.collection('products').get();
@@ -35,6 +36,50 @@ app.get('/products', (req, res) => __awaiter(void 0, void 0, void 0, function* (
             snapData.push({ id: doc.id, name: data.name, desc: data.desc, price: data.price });
         });
         res.status(200).send({ msg: "Ok", snapData });
+    }
+    catch (error) {
+        res.status(500).send({ msg: "Whoops! Something died", error });
+    }
+}));
+app.post('/products', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log('----- POST -----');
+    try {
+        const { name, desc, price } = req.body;
+        if (name.length < 3 || name.length > 100) {
+            res.status(403).send({ msg: "Name must be between 3 and 100 chars!" });
+        }
+        else if (desc.length < 5 || desc.length > 1000) {
+            res.status(403).send({ msg: "Description must be between 5 and 1000 chars!" });
+        }
+        else if (price < 1 || price > 20000) {
+            res.status(403).send({ msg: "Price must be in the range of 1 and 20,000!" });
+        }
+        else {
+            const snapshot = yield _db.collection('products').add({
+                name,
+                desc,
+                price
+            });
+            res.status(200).send({ msg: "Ok", product: { id: snapshot.id, body: req.body } });
+        }
+    }
+    catch (error) {
+        res.status(500).send({ msg: "Whoops! Something died", error });
+    }
+}));
+app.put('/products', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log('----- PUT -----');
+    try {
+        res.status(200).send({ msg: "Ok", todo: 'MISSING IMPLEMENTATION!' });
+    }
+    catch (error) {
+        res.status(500).send({ msg: "Whoops! Something died", error });
+    }
+}));
+app.delete('/products', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log('----- DELETE -----');
+    try {
+        res.status(200).send({ msg: "Ok", todo: 'MISSING IMPLEMENTATION!' });
     }
     catch (error) {
         res.status(500).send({ msg: "Whoops! Something died", error });
